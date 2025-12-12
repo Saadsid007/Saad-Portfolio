00632adb7a809e9435a121a2bd3e0dee748ecc5e
Saadsid007 <saadsiddiqui8532@gmail.com>
2025-12-12 23:33:01 +0530

    code fixed

--- prisma/migrations/20251212180020_init/migration.sql
+++ prisma/migrations/20251212180020_init/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "Message" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "subject" TEXT NOT NULL,
     "message" TEXT NOT NULL,
     "ip" TEXT,
     "userAgent" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
